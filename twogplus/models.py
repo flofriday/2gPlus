af20b8ba12b2b84db7d16ac8109aef1ade2e68f3
paulpaul168 <github@paulhoeller.at>
2021-12-12 18:07:56 +0100

    names not all caps anymore

--- twogplus/models.py
+++ twogplus/models.py
@@ -7,18 +7,21 @@ class User(db.Model):
 
     id: int = db.Column(db.Integer, primary_key=True)
     name: str = db.Column(db.Text, nullable=False)
-    created_at: datetime = db.Column(
-        db.Date, nullable=False, default=db.func.now()
-    )
+    created_at: datetime = db.Column(db.Date, nullable=False, default=db.func.now())
     is_tested: bool = db.Column(db.Boolean, nullable=False)
     is_vaccinated: bool = db.Column(db.Boolean, nullable=False)
 
-    def __init__(
-        self, name: str, is_vaccinated: bool = False, is_tested: bool = False
-    ):
+    def __init__(self, name: str, is_vaccinated: bool = False, is_tested: bool = False):
         self.name = name
         self.is_vaccinated = is_vaccinated
         self.is_tested = is_tested
 
     def __repr__(self):
         return f"<User id={self.id}, name={self.name}>"
+
+    def get_capitalized_name(self) -> str:
+        return (
+            self.name.split(" ")[0].capitalize()
+            + " "
+            + self.name.split(" ")[1].capitalize()
+        )
